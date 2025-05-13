b1fff18d5f9b2d739c0dd7ba286a657cfcf98ded
Shento Hendriks <shentohendriks@gmail.com>
2025-05-13 23:04:52 +0200

    added relative numbers

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,7 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Nvim Tree
-map("n", "<leader>ee", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle File explorer" })
-map("n", "<leader>ef", "<cmd> NvimTreeFindFile <CR>", { desc = "Toggle File explorer" })
+map("n", "<leader>ee", "<cmd> NvimTreeToggle <CR>", { desc = "nvimtree Toggle File explorer" })
+map("n", "<leader>ef", "<cmd> NvimTreeFindFile <CR>", { desc = "nvimtree Toggle File explorer opened file" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
